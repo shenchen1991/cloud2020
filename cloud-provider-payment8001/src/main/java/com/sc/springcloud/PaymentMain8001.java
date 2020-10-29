8eb5f801787f87badbf2d7c45ae2ee24912dbb21
shenchen <572631358@qq.com>
2020-10-29 10:24:36 +0800

    Initial commit

---
+++ cloud-provider-payment8001/src/main/java/com/sc/springcloud/PaymentMain8001.java
@@ -0,0 +1,18 @@
+package com.sc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: cloud
+ * @description: 启动打雷
+ * @author: 沈陈
+ * @create: 2020-10-28 19:26
+ **/
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+
+}
