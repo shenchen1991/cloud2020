c807a0afd4c344ab4f7339595e91bf8b96ae954c
shenchen <572631358@qq.com>
2020-11-02 18:58:42 +0800

    12

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/sc/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package com.sc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @program: cloud2020
+ * @description:
+ * @author: 沈陈
+ * @create: 2020-11-02 18:42
+ **/
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
